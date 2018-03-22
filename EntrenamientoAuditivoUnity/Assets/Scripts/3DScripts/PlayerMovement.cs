ad45adcb54e510e305ba33a23fef98eee782b85b
cpatin10 <catapf493@yahoo.com>
2018-03-22 09:09:02 -0500

    Correction in Piano Octave prefab
    The black keys in all prefabs are made longer so in order to evade the empty space that appears between black and white keys whe a white key is pressed

---
+++ EntrenamientoAuditivoUnity/Assets/Scripts/3DScripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float initialX;
+        float initialY;
+        float initialZ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
