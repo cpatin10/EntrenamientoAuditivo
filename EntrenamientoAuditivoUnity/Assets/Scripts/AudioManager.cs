0bea40fe1db7139e766ff9cdbbc2bc727b025b99
cpatin10 <catapf493@yahoo.com>
2018-03-30 08:31:14 -0500

    Adding dictionary for storing name of note with its corresponding id, for performance purposes

--- EntrenamientoAuditivoUnity/Assets/Scripts/AudioManager.cs
+++ EntrenamientoAuditivoUnity/Assets/Scripts/AudioManager.cs
@@ -5,12 +5,14 @@
 using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 //The AudioManager was built using the tutorial found at: https://www.youtube.com/watch?v=6OT43pvUyfY
 public class AudioManager : MonoBehaviour {
 
     public Sound[] sounds;
-
+    // Dictionary for storing the audioName (note name) with its id 
+    private Dictionary<string, int> notesId;
     public static AudioManager instance;
 
 	// Use this for initialization
@@ -26,6 +28,20 @@ public class AudioManager : MonoBehaviour {
 
         DontDestroyOnLoad(gameObject);
 
+        notesId = new Dictionary<string, int>(sounds.Length);
+
+        for (int i = 0; i < sounds.Length; ++i)
+        {
+            sounds[i].source = gameObject.AddComponent<AudioSource>();
+            sounds[i].source.clip = sounds[i].clip;
+
+            sounds[i].source.volume = sounds[i].volume;
+            sounds[i].source.loop = sounds[i].loop;
+
+            notesId.Add(sounds[i].name, i);
+        }
+
+        /*
         foreach (Sound sound in sounds) {
             sound.source = gameObject.AddComponent<AudioSource>();
             sound.source.clip = sound.clip;
@@ -33,6 +49,7 @@ public class AudioManager : MonoBehaviour {
             sound.source.volume = sound.volume;
             sound.source.loop = sound.loop;
         }
+        */
 	}
 
     // Looks for the Sound object with the given name, and calls its source to reproduce the sound
@@ -45,8 +62,15 @@ public class AudioManager : MonoBehaviour {
         s.source.Play();
     }
 
+    // Returns the sound given the int id that it has on the sounds array
     public Sound GetSoundByID(int id) {
         return sounds[id];
+    }
+
+    // Returns the sound id given its name
+    public int GetSoundIDByName(string name)
+    {
+        return notesId[name];
     }
 }
     
